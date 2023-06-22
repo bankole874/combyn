0c320b24ebf8d4e0bf64a8548355718f20f65eed
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2023-06-22 01:13:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const navEl = document.querySelector('.navigation');
+const navEl = document.querySelector('.navbar');
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 56) {
         navEl.classList.add('navbar-scrolled');
